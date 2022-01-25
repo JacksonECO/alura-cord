3b48d363f0cd425941335fec985ae1e6cb250c6e
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-01-25 12:02:26 -0300

    Aula 2; Apendendo a trabalhar com rotas (Router) e com "observable"; Reload de page; Componentes Globais

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+
+export default function MyApp({ Component, pageProps }) {
+    // Código comum em todas as telas
+    // Parece ser um componente global, e as tela ficam dentro deste
+    
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+
+function GlobalStyle() {
+    return (
+        <style global jsx>{`
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          list-style: none;
+        }
+        body {
+          font-family: 'Open Sans', sans-serif;
+        }
+        /* App fit Height */ 
+        html, body, #__next {
+          min-height: 100vh;
+          display: flex;
+          flex: 1;
+        }
+        #__next {
+          flex: 1;
+        }
+        #__next > * {
+          flex: 1;
+        }
+        /* ./App fit Height */ 
+      `}</style>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
-import {Box, Button, Text, TextField, Image } from '@skynexui/components'
+import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import React from 'react';
+import {useRouter} from 'next/router'
 import appConfig from '../config.json'
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
+
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -46,7 +21,7 @@ function Titulo(props) {
     )
 }
 
-// function HomePage() {
+// export default function HomePage() {
 //     return (
 //         <div>
 //             <GlobalStyle></GlobalStyle>
@@ -57,14 +32,14 @@ function Titulo(props) {
 //     )
 // }
 
-// export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'JacksonECO';
+    
+    const [username, setUsername] = React.useState('JacksonECO');
+    const router = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +66,13 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (event) {
+                            /// Não atualiza a tela, para que futuramente utilizar uma atualização de tela sem reload;
+                            event.preventDefault();
+                            /// Código padrão, funciona no console do navegador;
+                            // window.location.href = '/chat';
+                            router.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -111,7 +93,12 @@ export default function PaginaInicial() {
                                     backgroundColor: appConfig.theme.colors.neutrals[800],
                                 },
                             }}
+                            type="text" value={username} onChange={function (a) {
+                                setUsername(a.target.value);
+                            }}
                         />
+
+
                         <Button
                             type='submit'
                             label='Entrar'
